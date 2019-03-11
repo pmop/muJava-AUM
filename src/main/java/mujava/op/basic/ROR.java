e779a90bdc36c4febdf98b39fb872642e308fb7c
pedro <pmop@ic.ufal.br>
2019-03-11 17:55:57 -0300

    ROR13 ERule implementation.
    Signed-off-by: pedro <pmop@ic.ufal.br>

--- src/main/java/mujava/op/basic/ROR.java
+++ src/main/java/mujava/op/basic/ROR.java
@@ -55,49 +55,130 @@ public class ROR extends Arithmetic_OP {
 	}
 
 	public void visit(BinaryExpression p) throws ParseTreeException {
-		// term = if (vArgs.length op1 0)
-		// vArgs is string or array
-		// ROR(op1) -> op2
-		// when op1 is < and op2 is !=
-		// when op1 is != and op2 is <
-		// when op1 is > and op2 is !=
-		// when op1 is != and op2 is >
-		// when op1 is == and op2 is <=
-		boolean insideIf = false;
-		ParseTreeObject parent = p.getParent();
-		if (parent instanceof IfStatement) {
-			//Inside of if
-			insideIf = true;
-		}
+
+
 
 		Expression left = p.getLeft();
 		left.accept(this);
 		Expression right = p.getRight();
 		right.accept(this);
 
+		if (right instanceof Variable) {
+		}
+		//if (left instanceof )
+
 		int op_type = p.getOperator();
 
 		if (isArithmeticType(p.getLeft()) && isArithmeticType(p.getRight())) {
 			// fix the fault that missed <, Lin, 050814
-//          Possible refactoring for code below.
-//			switch (op_type) {
-//				case BinaryExpression.GREATER: case BinaryExpression.GREATEREQUAL:
-//				case BinaryExpression.LESSEQUAL: case BinaryExpression.EQUAL:
-//				case BinaryExpression.NOTEQUAL: case BinaryExpression.LESS:
-//					primitiveRORMutantGen(p,op_type);
-//			}
-
-			if ((op_type == BinaryExpression.GREATER) || (op_type == BinaryExpression.GREATEREQUAL)
-					|| (op_type == BinaryExpression.LESSEQUAL) || (op_type == BinaryExpression.EQUAL)
-					|| (op_type == BinaryExpression.NOTEQUAL) || (op_type == BinaryExpression.LESS)) {
-				primitiveRORMutantGen(p, op_type);
+//          This switch is a refactored version of the if clause below
+			switch (op_type) {
+				case BinaryExpression.GREATER: case BinaryExpression.GREATEREQUAL:
+				case BinaryExpression.LESSEQUAL: case BinaryExpression.EQUAL:
+				case BinaryExpression.NOTEQUAL: case BinaryExpression.LESS:
+					primitiveRORMutantGen(p,op_type);
 			}
+
+//			if ((op_type == BinaryExpression.GREATER) || (op_type == BinaryExpression.GREATEREQUAL)
+//					|| (op_type == BinaryExpression.LESSEQUAL) || (op_type == BinaryExpression.EQUAL)
+//					|| (op_type == BinaryExpression.NOTEQUAL) || (op_type == BinaryExpression.LESS)) {
+//				primitiveRORMutantGen(p, op_type);
+//			}
 		} else if ((op_type == BinaryExpression.EQUAL) || (op_type == BinaryExpression.NOTEQUAL)) {
 			objectRORMutantGen(p, op_type);
 		}
 	}
 
+	private boolean isEquivalent() {
+		boolean isEq = false;
+		return isEq;
+	}
+
+	boolean isEquivalent(BinaryExpression exp, int op1, int op2) {
+
+		boolean e_rule_13 = false;
+
+		/*
+			ROR E-Rule 13
+			term = if (vArgs.length op1 0)
+			vArgs is string or array
+			ROR(op1) -> op2
+			when op1 is != and op2 is <
+			when op1 is != and op2 is >
+			when op1 is > and op2 is !=
+			when op1 is < and op2 is !=
+			when op1 is == and op2 is <=
+
+			>>>>>
+		*/
+		try {
+
+			boolean insideIf = exp.getParent() instanceof IfStatement;
+			Expression left = exp.getLeft();
+			Expression right = exp.getRight();
+			OJClass leftOJClass = this.getType(left);
+			OJClass rightOJClass = this.getType(right);
+
+			boolean leftIsVariable = leftOJClass == OJSystem.INT && (left instanceof Literal);
+			boolean rightIsVariable = rightOJClass == OJSystem.INT && (right instanceof Literal);
+			boolean leftIsContainer = (leftOJClass == OJSystem.STRING) || leftOJClass.isArray();
+			boolean rightIsContainer = (rightOJClass == OJSystem.STRING) || rightOJClass.isArray();
+			boolean containsLengthMethodCall = false;
+			boolean containsZeroInteger = false;
+
+			if (leftIsVariable) {
+				Literal leftLiteral = (Literal) left;
+				containsZeroInteger = leftLiteral.getLiteralType() == 0;
+			} else if (rightIsContainer) {
+				Literal rl = (Literal) right;
+				containsZeroInteger = rl.getLiteralType() == 0;
+			}
+			if (leftIsContainer) {
+				if (left instanceof MethodCall) {
+					MethodCall mc = (MethodCall) left;
+					if (mc.getName().equals("length")) containsLengthMethodCall = true;
+				}
+			} else if (rightIsContainer) {
+				if (right instanceof MethodCall) {
+					MethodCall mc = (MethodCall) right;
+					if (mc.getName().equals("length") ) containsLengthMethodCall = true;
+				}
+			}
+
+			if (insideIf) {
+				if ((leftIsContainer && rightIsVariable) || (leftIsVariable && rightIsContainer)) {
+					if (containsLengthMethodCall) {
+						e_rule_13 = true;
+						switch (op1) {
+							case BinaryExpression.NOTEQUAL:
+								if (op2 == BinaryExpression.LESS || op2 == BinaryExpression.GREATER)
+								break;
+							case BinaryExpression.GREATER:
+								if (op2 == BinaryExpression.NOTEQUAL)
+								break;
+							case BinaryExpression.LESS:
+								if (op2 == BinaryExpression.NOTEQUAL)
+								break;
+							case BinaryExpression.EQUAL:
+								if (op2 == BinaryExpression.LESSEQUAL)
+								break;
+							default:
+								e_rule_13 = false;
+								break;
+						}
+					}
+				}
+			}
+
+		} catch (ParseTreeException e) {
+			System.err.println("ParseTreeException on ROR13 E-Rule: "+e.toString());
+		}
+
+		return e_rule_13;
+	}
+
 	private void primitiveRORMutantGen(BinaryExpression exp, int op) {
+
 		BinaryExpression mutant;
 
 		/**
@@ -341,8 +422,8 @@ public class ROR extends Arithmetic_OP {
 	/**
 	 * Avoid generate duplicated mutants
 	 * 
-	 * @param statement
-	 * @param mutation
+	 * @param exp
+	 * @param mutant
 	 * @return
 	 * @throws ParseTreeException
 	 */
